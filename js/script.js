f37a2568992cf09e3faf14e13a32876e6883e94c
4m3r <bladeklub@gmail.com>
2018-03-03 22:53:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new SlideNav();
+
+var nav = new SlideNav({
+    activeClass: "active",
+    toggleButtonSelector: true,
+    toggleBoxSelector: false,
+    hideAfterSelect: true,
+    speed: 10,
+    changeHash: false,
+    navBoxToggleClass: false
+});
